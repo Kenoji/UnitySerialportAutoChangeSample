b9887b86153c788453f295c3f5f2975e059a62c4
DESKTOP-E1NPR4T\tanik <tanizakik_date@yahoo.co.jp>
2017-02-24 10:52:08 +0900

    別のプロジェクトから移動してきた(ちゃんとした動確はまだ)

---
+++ Assets/Scripts/ReceiveSample.cs
@@ -0,0 +1,21 @@
+﻿    using UnityEngine;
+using System.Collections;
+
+public class ReceiveSample : MonoBehaviour
+{
+    public SerialHandler sh;
+	// Use this for initialization
+	void Start ()
+	{
+	    sh.OnDataReceived += OnDataReceived;
+	}
+
+    void OnDataReceived(string message)
+    {
+        Debug.Log(message);
+    }
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
